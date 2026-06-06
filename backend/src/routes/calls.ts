bba4e89b34ec8c311bd7393cded47e5dd8d13214
Abinavdev A D <70625967+abinavdev@users.noreply.github.com>
2026-06-06 16:53:56 +0530

    Updated calls.ts

--- backend/src/routes/calls.ts
+++ backend/src/routes/calls.ts
@@ -1,15 +1,11 @@
 import { Router } from 'express';
-import * as callsController from '../controllers/calls';
+import { getCallLogs, getCallLog } from '../controllers/calls';
 import { authenticateUser } from '../middleware/auth';
 
 const router = Router();
 
-console.log('CALLS CONTROLLER EXPORTS:', Object.keys(callsController));
-
 router.use(authenticateUser);
-
-router.get('/', callsController.getCallLogs);
-router.get('/:id', callsController.getCallLog);
-router.post('/', callsController.createCallLog);
+router.get('/', getCallLogs);
+router.get('/:id', getCallLog);
 
 export default router;
